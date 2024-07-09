04b52128390189f7fea98be6f3097b692b1acab1
SadayukiKoga <wezarius@gmail.com>
2024-07-09 20:35:50 +0900

    read機能を追加

--- api/server.ts
+++ api/server.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 
 import { applyServerSettings } from './settings';
@@ -10,8 +11,50 @@ applyServerSettings(app);
 
 // ↓↓↓ バックエンド処理を記述して実際に開発してみましょう！！
 
+declare global {
+  // eslint-disable-next-line no-var
+  var __db__: PrismaClient | undefined;
+}
+
+const initPrisma = () => {
+  if (process.env.NODE_ENV === 'production') return new PrismaClient();
+
+  const db = (global.__db__ = global.__db__ ?? new PrismaClient());
+  db.$connect();
+  return db;
+};
+
+const prisma = initPrisma();
+
+app.get('/memos', async (req, res) => {
+  const records = await prisma.memo.findMany();
+
+  const memos = records.map((memo) => {
+    return {
+      id: memo.id,
+      title: memo.title,
+      createdAt: formatDateInJa(memo.createdAt),
+    };
+  });
+
+  res.json({ data: memos });
+});
+
+const formatDateInJa = (date: Date) => {
+  const year = date.getFullYear().toString();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  return `${year}/${month}/${day} ${hours}:${minutes}`;
+};
+
+
 // ↑↑↑ バックエンド処理を記述して実際に開発してみましょう！！
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
+
